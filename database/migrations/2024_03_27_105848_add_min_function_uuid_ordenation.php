f5aa7dccb506567b3c23c60a7fc3d5082dd9a78b
Fabiano Fernandes <contato@fabianofernandes.adm.br>
2024-03-27 11:21:30 -0300

    style: styling code

--- database/migrations/2024_03_27_105848_add_min_function_uuid_ordenation.php
+++ database/migrations/2024_03_27_105848_add_min_function_uuid_ordenation.php
@@ -1,25 +1,21 @@
 <?php
 
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
 
-return new class extends Migration
-{
+return new class() extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-
-        DB::statement("
+        DB::statement('
             create or replace function min(uuid, uuid) returns uuid immutable parallel safe language plpgsql as $$ begin return least($1, $2); end $$;
-        ");
+        ');
 
-        DB::statement("
+        DB::statement('
             create or replace aggregate min(uuid) (sfunc = min, stype = uuid, combinefunc = min, parallel = safe, sortop = operator (<));
-        ");
+        ');
     }
 
     /**
@@ -27,14 +23,12 @@ return new class extends Migration
      */
     public function down(): void
     {
-        DB::statement("
+        DB::statement('
             drop aggregate min(uuid)
-        ");
+        ');
 
-        DB::statement("
+        DB::statement('
             drop function min(uuid, uuid)
-        ");
-
-
+        ');
     }
 };
--- database/migrations/2024_03_27_111149_add_max_function_uuid_ordenation.php
+++ database/migrations/2024_03_27_111149_add_max_function_uuid_ordenation.php
@@ -1,24 +1,21 @@
 <?php
 
-use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\DB;
 
-return new class extends Migration
-{
+return new class() extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        DB::statement("
+        DB::statement('
         create or replace function max(uuid, uuid) returns uuid immutable parallel safe language plpgsql as $$ begin return greatest($1, $2); end $$;
-        ");
+        ');
 
-        DB::statement("
+        DB::statement('
         create or replace aggregate max(uuid) ( sfunc = max, stype = uuid, combinefunc = max, parallel = safe, sortop = operator (>));
-        ");
+        ');
     }
 
     /**
@@ -26,12 +23,12 @@ return new class extends Migration
      */
     public function down(): void
     {
-        DB::statement("
+        DB::statement('
             drop aggregate max(uuid)
-        ");
+        ');
 
-        DB::statement("
+        DB::statement('
             drop function max(uuid, uuid)
-        ");
+        ');
     }
 };
